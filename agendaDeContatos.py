37fc8662e7bc8286a7d6be8c0497d802d64d5b85
Danrlei Kemper <danrleikemper95@gmail.com>
2024-02-14 20:25:31 -0300

    Adicionado a função adicionar_contatos

--- agendaDeContatos.py
+++ agendaDeContatos.py
@@ -1,9 +1,18 @@
+#Finção para adicionar os dados do contato
+def adicionar_contato(contatos, nome_contato, telefone_contato, email_contato):
+    novo_contato = {"Nome": nome_contato, "Telefone": telefone_contato, "E-mail": email_contato}
+    contatos.append(novo_contato)
+    #print(f"Contato: {nome_contato} {telefone_contato} {email_contato} foi adicionado com sucesso!")
+    print("Contato foi adicionado com sucesso!")
+    return
+
+
 #Variavel para armazenar os contatos / criação de uma lista
 contatos = []
 
 #Menu da Agenda
 while True:
-    print("Bem Vindo a Sua agenda de contatos\n")
+    print("\nBem Vindo a Sua agenda de contatos\n")
     print("##### MENU ####")
 
     print("1 - Adicionar um contato")
@@ -17,7 +26,13 @@ while True:
 
     escolha = input("Digite sua escolha: ")
 
-    if escolha == "8":
+    if escolha == "1":
+        nome_contato = input("Nome: ")
+        telefone_contato = input("Telefone: ")
+        email_contato = input("E-mail: ")
+        adicionar_contato(contatos, nome_contato, telefone_contato, email_contato)
+
+    elif escolha == "8":
         print("PROGRAMA FINALIZADO!")
         break
 
